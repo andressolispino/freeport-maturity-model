29c945f81a7ed8ab9381edc987cfb2587e88b4c5
Andrés Felipe Solis Pino <afsolis@unicauca.edu.co>
2026-05-06 16:36:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
 
-const GROQ_API_KEY = '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'; // GROQ API KEY
+const GROQ_API_KEY = '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'; // GROQ API KEY
 const GROQ_MODEL = 'llama-3.3-70b-versatile'; // SELECTED MODEL
 
 // --- SUPABASE CONFIGURATION ---
